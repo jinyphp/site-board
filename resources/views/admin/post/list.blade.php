de91f096ac784399bdc94dec1cd027072aaee133
hojinlee <infohojin@gmail.com>
2024-08-07 19:09:02 +0900

    post update

--- resources/views/admin/post/list.blade.php
+++ resources/views/admin/post/list.blade.php
@@ -2,10 +2,10 @@
     {{-- 테이블 제목 --}}
     <x-wire-thead>
         <th width='200'>코드</th>
-        <th width='100'>포스트</th>
-        <th>타이틀</th>
 
-        <th width='200'>담당자</th>
+        <th>타이틀</th>
+        <th width='100'>조회수</th>
+        <th width='200'>작성자</th>
         <th width='200'>등록일자</th>
     </x-wire-thead>
     <tbody>
@@ -14,23 +14,25 @@
             {{-- 테이블 리스트 --}}
             <x-wire-tbody-item :selected="$selected" :item="$item">
                 <td width='200'>
-                    <div class="d-flex gap-2">
-
-                    </div>
-                </td>
-                <td width='100'>
-                    <a href="/post/{{$item->code}}" target="_blank">
-                        {{$item->post}}
+                    <a href="/post/{{$item->id}}" target="_blank">
+                        {{$item->code}}
                     </a>
                 </td>
+
                 <td>
                     <x-click wire:click="edit({{$item->id}})">
                         {{$item->title}}
                     </x-click>
 
                 </td>
+                <td width='100'>
+                    {{$item->click}}
+                </td>
 
-                <td width='200'>{{$item->manager}}</td>
+                <td width='200'>
+                    <div>{{$item->name}}</div>
+                    <div>{{$item->email}}</div>
+                </td>
                 <td width='200'>{{$item->created_at}}</td>
             </x-wire-tbody-item>
             @endforeach
--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -44,4 +44,31 @@ if(!function_exists("getPosts")) {
 }
 
 
+/**
+ * 계시판 목록을 반환합니다.
+ */
+if(!function_exists("getBoards")) {
+    function getBoards($limit=5, $code=null) {
+        $db = DB::table('site_board')->orderBy('id', 'desc');
+
+        if($code) {
+            if(is_array($code)) {
+                foreach($code as $key => $value) {
+                    $db->where($key,$value);
+                }
+            } else {
+                $db->where('code',$code);
+            }
+        }
+
+        if(is_array($limit)) {
+            $db->offset($limit[0]-1)  // Skip the first 9 records
+            ->limit($limit[1]);   // Fetch the next 8 records (10th to 17th)
+        } else {
+            $db->limit($limit);
+        }
+
+        return $db->get();
+    }
+}
 
